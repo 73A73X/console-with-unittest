4863dd12402b5e0cb1b6b2f66f9424838e860f33
Aleksi Tuukkanen <caifudigital@gmail.com>
2022-09-19 10:27:18 +0300

    Yeah

---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        {
+            Assert.Equal(11, sum(5, 6));
+        }
+
+    }
+    [Fact]
+    public void Test2()
+    {
+        {
+            Assert.Equal(12, sum(5, 6));
+        }
+
+    }
+
+    int sum(int a, int b){
+        return a+b;
+    }
+}
